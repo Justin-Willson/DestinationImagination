b1be72e5c8ae0d4bc49d62f03a0d20aeac5c2534
Justin Willson <jt.will.525@gmail.com>
2020-05-07 13:40:56 -0600

    initial commit

---
+++ DigitalDI/BoxAndBall.js
@@ -0,0 +1,54 @@
+function setup() {
+    rectMode(CENTER);
+    noStroke();
+  
+    //This line of code creates a 400 pixel by 400 pixel box
+    //What do you think will happen if we change the first number?
+    createCanvas(400, 400);    
+}
+
+function draw() {
+    //This line of code sets the background color. We are using the Red
+    //Green Blue(RGB) color system in our code. It uses numbers to 
+    //represent colors. DI purple is (129, 61, 214).
+    //Find cool colors using this link:
+    //https://www.google.com/search?q=color+picker
+    background(23, 128, 35);
+    
+    //Our background is set, lets draw and color shapes!
+
+    //The fill() function sets the color of shapes we draw.
+    //We chose RGB numbers to make DI orange (255,102,0).
+    //Rect is short for Rectangle
+    fill(25, 10, 0);   
+    rect(200, 300, 100, 80);
+
+
+    //Ellipse is a fancy word for an oval.
+    fill(144, 87, 171);   
+    ellipse(300, 60, 200, 200);
+    fill(14, 87, 171);   
+    ellipse(200, 60, 100, 200);
+}
+
+
+//--------------------------------Challenges------------------------------------
+/*  Mild: 
+        1. Change the color of the background to be your favorite color.
+        2. Change the color of the square to yellow.
+        3. Move the circle so it is no longer in the center of the canvas. 
+        4. Make the circle into an oval that is 50 pixels wide and 30 pixels tall.      
+    Medium: 
+        1. Move the square so that it's in the bottom right corner.
+        2. Make the circle and square two different colors.
+        3. Add another square and another circle to the screen.
+        3. Put a small square inside a large circle. Make sure the two shapes
+           are different colors.
+    Spicy:
+        1. Make a smiley face
+        2. Make the DI box and ball
+        3. Make a snow man.
+        4. Look at the reference for the rect() function. It teaches you how to
+           make rounded squares (the mathematical term for these is squircle).
+           Make a squircle on the screen in your favorite color. https://p5js.org/reference/#/p5/rect
+*/
---
+++ DigitalDI/Setup.js
@@ -0,0 +1,66 @@
+/*
+    Digital DI Programming: Setup
+
+    Welcome to the Digital DI programming class! In this lesson, 
+    we will learn how to make the computer run our code. For this
+    class, we will be using a programming language called javascript
+    as well as a library called P5.js. We're doing this to get familiar
+    with the environment we will be using in the lesson Let's get started!
+
+    Vocab:
+        Programing Language: A programming language is a collection of
+                             human readable operations that a computer
+                             can understand. There are many different 
+                             programming languages such as javascript,
+                             C, and python. In our class we will be 
+                             using javascript.
+        Library: A library is a collection of code someone else has 
+                 already written for you. By using a library, we
+                 can avoid "reinvinting the wheel" every time we 
+                 want to write a new program. We can also view a library
+                 as adding extra functionality to our programming language
+                 For example, in our class, we will use a library called
+                 P5.js, which adds the ability to draw pictures using 
+                 code.
+        Comment: A comment allows us as programmers to tell the computer
+                 NOT to read part of our code. This allows us to explain
+                 our code to the reader. In javascript, we can make a single
+                 line comment by typing //, or a multi-line comment by 
+                 putting everything we want to say in between two /* 's like
+                 I've done for this section. This is super important because 
+                 the computer doesn't understand english, so everything I've
+                 written would confuse it.
+        string: A string is text that you want the computer to know about. In
+                javasctipt, a string is denoted by wrapping the words you want
+                in quotation marks. For example "Hello World!" is a string. If
+                you don't put the quotation marks around it, the computer gets
+                confused because it's trying to understand the words you wrote
+                as code rather than as words.
+*/
+
+/*---------------------------------Code----------------------------------------*/
+/*
+    In order to begin coding, we need a place to run our code. Open up your
+    web browser and navigate to https://editor.p5js.org/. Here, you should 
+    see a "play" button, a box with some code in it, a box labeled "preview",
+    and a box labled "Console". Delete the code in the code box and copy
+    all the code in this file that is below the dashed line, then paste 
+    the code you copied into the box on the website. From now on, you 
+    
+    Next, press the play
+    button, you should see the words "Hello World!" pop up in the Console box.
+*/
+
+/*
+This line of code tells the computer to write everything in the parentheses
+to the console. Here, we're having the computer write "Hello World!"
+*/
+console.log("Hello World!");
+
+/*--------------------------------Challenges----------------------------------*/
+/*  Mild: 
+        1. Modify the line of code above so that the code outputs your name.
+        2. Add a comment to the code above saying that you edited the code
+    Medium: 
+        1. Modify the code so that it prints two or more different lines.
+*/
